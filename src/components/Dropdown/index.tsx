4e72112fcbd1a2cbe9b6867bedf01be8dfbe11a0
Eric Do <eric.do@go1.com>
2021-08-22 15:18:42 +1000

    skill data

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -5,6 +5,7 @@ import Input from "@material-ui/core/Input";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
+import { TextField } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -38,16 +39,38 @@ const MenuProps = {
 };
 
 const names = [
-  "Oliver Hansen",
-  "Van Henry",
-  "April Tucker",
-  "Ralph Hubbard",
-  "Omar Alexander",
-  "Carlos Abbott",
-  "Miriam Wagner",
-  "Bradley Wilkerson",
-  "Virginia Andrews",
-  "Kelly Snyder",
+  "Calculate the square footage of a new home under construction based on plans using scales and ratios",
+  "Use software on a portable device to document a building inspection by recording measurements, checking compliance and uploading photos",
+  "Share knowledge, experience, information and resources with others as an integral part of work relationships",
+  "Write a detailed literature review",
+  "Read and analyse information comparing the impact of downturns in the global economy on the cost of producing and selling beef",
+  "Develop and implement a new business strategy based on changing market conditions",
+  "Examine records and data to investigate and prosecute corporate criminals",
+  "Explain foot care procedures to a diabetic patient",
+  "Prioritise and plan multiple complex tasks several months ahead",
+  "Collaborate with others to prepare and perform artistic productions",
+  "Create charts and graphs to accurately convey the results of a customer satisfaction survey",
+  "Use software on a portable device to document a building inspection by recording measurements, checking compliance and uploading photos",
+  "Share knowledge, experience, information and resources with others as an integral part of work relationships",
+  "Write a detailed literature review",
+  "Read a book on leading and managing change in the workplace",
+  "Determine the impact of new menu changes on a restaurant's purchasing requirements",
+  "Redesign a floor layout to take advantage of new manufacturing techniques",
+  "Participate in a work meeting and ask appropriate follow up questions ",
+  "Develop a program of multiple live musical performances, special events and sports carnivals throughout the year at a sports club venue and coordinate volunteers, staff and contractors to deliver the events",
+  "Negotiate a price and sales terms then prepare a contract for a house",
+  "Calculate the square footage of a new home under construction based on plans using scales and ratios",
+  "Recognise different ways to connect to the internet (e.g. Bluetooth, Wi-Fi, hotspot)",
+  "Recognise and avoid behaviours, like unkind gossip, that undermine effective group interaction",
+  "Write a memo to staff outlining new directives",
+  "Read a memo from management describing new personnel policies",
+  "Determine the impact of new menu changes on a restaurant's purchasing requirements",
+  "Redesign a floor layout to take advantage of new manufacturing techniques",
+  "Give instructions to a lost driver",
+  "Prioritise and plan multiple complex tasks several months ahead",
+  "Negotiate a price and sales terms then prepare a contract for a house",
+  "Calculate the square footage of a new home under construction based on plans using scales and ratios",
+  "Recognise different ways to connect to the internet (e.g. Bluetooth, Wi-Fi, hotspot)"
 ];
 
 const Dropdown: React.FC<any> = ({ handleChange, value }) => {
@@ -55,23 +78,13 @@ const Dropdown: React.FC<any> = ({ handleChange, value }) => {
 
   return (
     <div>
-      <FormControl className={clsx(classes.formControl, classes.noLabel)}>
-        <Select
-          displayEmpty
-          value={value}
-          onChange={handleChange}
-          input={<Input />}
-          MenuProps={MenuProps}
-          inputProps={{ "aria-label": "Without label" }}
-          variant='filled'
-        >
-          {names.map((name) => (
-            <MenuItem key={name} value={name}>
-              {name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+      <TextField select style={{ width: 150, marginTop: 12, marginLeft: 12 }}>
+        {names.map((name) => (
+          <MenuItem key={name} value={name}>
+            {name}
+          </MenuItem>
+        ))}
+      </TextField>
     </div>
   );
 };
